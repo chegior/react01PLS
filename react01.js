5979271aeb14ce4bf12fa3614a10777e709dd127
chegior <chegiorca@hotmail.com>
2017-08-06 13:35:53 -0600

    initial Project

---
+++ react01.js
@@ -0,0 +1,11 @@
+/**@jsx React.DOM */
+
+var Hello = React.createClass({ //create a component
+    render:function() { ///you have to supply a function
+        return <div>
+                    <h1>Hello REACT </h1>
+
+                </div>
+    }//the return is process at the JSX VS Reac
+
+});
